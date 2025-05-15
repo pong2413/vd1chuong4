41ec447b960ab509c1ea120e207849dc48d05571
pong2413 <phong24a3@gmail.com>
2025-05-15 08:47:42 +0700

    Add files via upload

---
+++ vd1spring/src/main/java/com/example/demo/HelloController.java
@@ -0,0 +1,10 @@
+package com.example.demo;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/hello")
+    public String hello() {
+        return "Hello World!";
+    }
+}
---
+++ vd1spring/src/main/java/com/example/demo/Vd1springApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Vd1springApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Vd1springApplication.class, args);
+	}
+
+}
